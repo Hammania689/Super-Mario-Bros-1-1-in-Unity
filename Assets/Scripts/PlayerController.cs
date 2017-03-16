c145c81ea519535b73178beb3dba86c8dd540628
Hameed Abdul <hameedabdulrashid@gmail.com>
2017-03-15 21:04:08 -0500

    Super Mario Bros Tutorial 13

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float height = 5f;
     public static int PlayerState;
-    public bool grounded = false;
+
     Animator anim;
-    bool movingRight;
+    bool movingRight, movingLeft, walking, jumped,
+        jumping, grounded = false;
+    float speed = 5f, height = 8.7f, jumpTime, walkTime;
+    int moveState;
     Rigidbody2D rb;
 
     void Start()
@@ -22,27 +23,86 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         anim.SetBool("movingRight", movingRight);
+        State();
     }
 	
 	void FixedUpdate ()
     {
+        if (!Input.GetKey(KeyCode.RightArrow) || !(Input.GetKey(KeyCode.D)) ||
+            (!Input.GetKey(KeyCode.LeftArrow) || (!Input.GetKey(KeyCode.A))))
+            moveState = 0;
         if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
+            moveState = 1;
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
+            moveState = 2;
+            Jump();
+    }
+
+    void State()
+    {
+        switch(moveState)
         {
-            transform.Translate(Vector3.right * speed * Time.fixedDeltaTime);
-            movingRight = true;
+            case 1: Movement(Vector3.right);
+                movingRight = true;
+                movingLeft = false;
+                break;
+            case 2: Movement(Vector3.left);
+                movingLeft = true;
+                movingRight = false;
+                break;
+            default:
+                movingRight = false;
+                movingLeft = false;
+                walking = false;
+                walkTime = 0f;
+                speed = 5f;
+                break;
         }
-        else movingRight = false;
-        if(Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
-            transform.Translate(Vector3.left * speed * Time.fixedDeltaTime); 
-        if (Input.GetKeyDown(KeyCode.UpArrow)  || (Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.Space))))
-            if(grounded == true)
-             rb.velocity = new Vector2 (0f, height); 
+
+        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.Space))))
+            {
+                jumped = true; jumping = true;
+                jumpTime += Time.fixedDeltaTime;
+            }
+            else jumping = false;
+    }
+
+    void Movement(Vector3 dir)
+    {
+        walking = true;
+        speed = Mathf.Clamp(speed, speed, 8f);
+        walkTime += Time.deltaTime;
+        transform.Translate(dir * speed * Time.fixedDeltaTime);
+        if (walkTime < 3f && walking)
+            speed += .025f;
+        else if (walkTime > 3f)
+            speed = 8f;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.Space))))
+            if (grounded == true)
+            rb.velocity = new Vector2(rb.velocity.x, height);
+
+        if (jumping == true && jumped == true)
+            rb.gravityScale -= (.1f * Time.fixedDeltaTime);
+        if (jumpTime > 1f)
+            jumping = false;
+        if (jumping == false && jumped == true)
+            rb.gravityScale += .2f;
     }
 
     void OnTriggerStay2D( Collider2D other)
     {
-        if(other.gameObject.tag == "Ground" || other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Enemy")
+        {
             grounded = true;
+            jumped = false;
+            jumping = false;
+            jumpTime = 0f;
+            rb.gravityScale = 1;
+        }
     }
 
     void OnTriggerExit2D( Collider2D other)
---
+++ Overview.md
@@ -0,0 +1,31 @@
+# Super Mario Bros World 1-1 Tutorial in Unity
+Recreation of the World 1 Stage 1 in Unity using C#
+	
+### YouTube Tutorial Done Right
+
+Thank you for showing your interest. Hopefully these tutorials will help us both in Unity :)
+
+# Tutorial Overview (Tutorial 9 and On)
+			
+Bear with me the early tutorials are inconsistent and vary in quality so lets skip to the good stuff
+##### All Tutorials So Far
+	https://www.youtube.com/watch?v=vgq0n_68uMc&list=PLDj1WczptC7gdLlrP7j8cfVy-kTn2sHDJ
+
+
+### Tutorial 13 
+#### Fixed Wonky Jumping, Walk to Speed, and More 2D Assets to Come
+
+![image](https://cloud.githubusercontent.com/assets/20171200/21540379/8d5aaea8-cd74-11e6-9039-9f97624c0c9d.png)
+
+
+![image](https://cloud.githubusercontent.com/assets/20171200/21540172/1aac6f64-cd73-11e6-8879-5b2c8d4ca71b.png)
+
+
+
+
+
+
+
+
+
+
